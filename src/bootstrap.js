f19af2584613fec54184040fa101fb3ce7859b04
farzinkakh <farzinask1996@gmail.com>
2024-02-12 21:04:36 +0330

    enpoint set from html file

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,7 +3,7 @@ import axios from "axios";
 window.axios = axios;
 
 // Set axios default endpoint
-window.axios.defaults.baseURL = 'http://185.50.37.66:1112';
+window.axios.defaults.baseURL = window._shared.endpoint;
 
 // Set axios default configs
 // window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
